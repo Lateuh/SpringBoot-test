5d79f352c632e7b47f8a4d6c24aa891e9215ac27
Lateuh <alexy.lefevre@gmail.com>
2020-11-25 18:06:51 +0100

    user crud and var env

--- src/main/java/com/example/SpringBoottest/controllers/LocationsController.java
+++ src/main/java/com/example/SpringBoottest/controllers/LocationsController.java
@@ -1,13 +1,38 @@
 package com.example.SpringBoottest.controllers;
 
+import com.example.SpringBoottest.models.Location;
+import com.example.SpringBoottest.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.SpringBoottest.repositories.LocationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/locations")
 public class LocationsController {
     @Autowired
     private LocationRepository locationRepository;
+
+    //renvoie liste de tous les utilisateurs
+    @GetMapping
+    public ResponseEntity<List<Location>> getUsers(){
+        List<Location> listLocations = new ArrayList<>();
+        try{
+            locationRepository.findAll().forEach(listLocations::add);
+            if(listLocations.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(listLocations,HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
--- src/main/java/com/example/SpringBoottest/controllers/UsersController.java
+++ src/main/java/com/example/SpringBoottest/controllers/UsersController.java
@@ -4,10 +4,13 @@ import com.example.SpringBoottest.models.User;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.SpringBoottest.repositories.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -17,37 +20,87 @@ public class UsersController {
 
     //renvoie liste de tous les utilisateurs
     @GetMapping
-    public List<User> getUsers(){
-        System.out.println("LAAAAAAAASIONJ?OINDOIUANDOUINAZODINAZOINDOINDOIEJNFUIO");
-        return userRepository.findAll();
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> listUsers = new ArrayList<>();
+        try{
+            userRepository.findAll().forEach(listUsers::add);
+            if(listUsers.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(listUsers,HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
-    @GetMapping
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public User getUser(@PathVariable long id){
-        return userRepository.getOne(id);
+    //renvoie l'utilisateur avec l'id entré dans URL
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") long id){
+        Optional<User> _user = userRepository.findById(id);
+        if (_user.isPresent()){
+         return new ResponseEntity<>(_user.get(),HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
+    //renvoie liste de tous les utilisateurs
+    @GetMapping("/rand")
+    public ResponseEntity<User> getUserRand(){
+        List<User> listUsers = new ArrayList<>();
+        try{
+            userRepository.findAll().forEach(listUsers::add);
+            if(listUsers.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            int randomIndex = (int) (Math.random() * listUsers.size());
+            User userRand = listUsers.get(randomIndex);
+            return new ResponseEntity<>(userRand,HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // ajoute un nouveau user dans la bdd
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public User postUser(@RequestBody final User user){
-        return userRepository.saveAndFlush(user);
+    public ResponseEntity<User> postUser(@RequestBody final User user){
+        return new ResponseEntity<>(userRepository.saveAndFlush(user),HttpStatus.CREATED);
     }
 
-
-    @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
-    public void delete(@PathVariable long id){
-        userRepository.deleteById(id);
+    //supprime l'user d'après son id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") long id){
+        try{
+            userRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @RequestMapping(value = "{id}",method = RequestMethod.PUT)
-    public User update(@PathVariable long id, @RequestBody User user){
-        // TO DO
-
-        User existingUser = userRepository.getOne(id);
-        BeanUtils.copyProperties(user,existingUser,"user_id");
-        return userRepository.saveAndFlush(existingUser);
+    //maj l'user avec l'id donné
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable("id") long id, @RequestBody User user){
+        if (user.getFirst_name().isEmpty() || user.getLast_name().isEmpty() ||
+                user.getEmail().isEmpty() || user.getPassword().isEmpty() ||
+                user.getPhone_number().isEmpty()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Optional<User> existingUser = userRepository.findById(id);
+        if(existingUser.isPresent()){
+            User _user = existingUser.get();
+            _user.setFirst_name(user.getFirst_name());
+            _user.setLast_name(user.getLast_name());
+            _user.setEmail(user.getEmail());
+            _user.setPassword(user.getPassword());
+            _user.setPhone_number(user.getPhone_number());
+            _user.setLocations(user.getLocations());
+            return new ResponseEntity<>(userRepository.saveAndFlush(_user),HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        //BeanUtils.copyProperties(user,existingUser,"user_id"); // utilisé dans le cours
     }
 }
